b4507e386ac12d97766eaa9331edca35cf416dfb
GregC <gcampecino@gmail.com>
2025-05-12 15:58:29 +0800

    docker github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (<div>Home</div>
-  );
+  console.log(process.env.NEXT_PUBLIC_API_URL)
+  return (<div>Home{process.env.NEXT_PUBLIC_API_URL}</div>);
 }
